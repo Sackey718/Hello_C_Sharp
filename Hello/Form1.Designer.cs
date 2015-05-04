ad90e297b5381722a9e84db6de58e066e6d07f0a
Sackey718 <yuuki718@gmail.com>
2015-05-04 19:04:56 +0900

    ダイアログボックス表示

--- Hello/Form1.Designer.cs
+++ Hello/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonHello
+            // 
+            this.buttonHello.Location = new System.Drawing.Point(12, 85);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(260, 71);
+            this.buttonHello.TabIndex = 0;
+            this.buttonHello.Text = "Hello";
+            this.buttonHello.UseVisualStyleBackColor = true;
+            this.buttonHello.Click += new System.EventHandler(this.buttonHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonHello);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonHello;
     }
 }
 
--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -16,5 +16,10 @@ namespace Hello
         {
             InitializeComponent();
         }
+
+        private void buttonHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello C#!!", "はじめまして");
+        }
     }
 }
