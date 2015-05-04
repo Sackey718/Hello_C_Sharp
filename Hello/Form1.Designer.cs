79fadea5dddeae61ab4766370f5829d35d6fbbe8
Sackey718 <yuuki718@gmail.com>
2015-05-04 20:12:39 +0900

    ボタン等を配置

--- Hello/Form1.Designer.cs
+++ Hello/Form1.Designer.cs
@@ -32,13 +32,19 @@
             this.buttonHello = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button_Exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_Time = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonHello
             // 
-            this.buttonHello.Location = new System.Drawing.Point(12, 12);
+            this.buttonHello.Location = new System.Drawing.Point(12, 171);
             this.buttonHello.Name = "buttonHello";
-            this.buttonHello.Size = new System.Drawing.Size(260, 115);
+            this.buttonHello.Size = new System.Drawing.Size(260, 43);
             this.buttonHello.TabIndex = 0;
             this.buttonHello.Text = "Hello";
             this.buttonHello.UseVisualStyleBackColor = true;
@@ -51,24 +57,84 @@
             // 
             // button_Exit
             // 
-            this.button_Exit.Location = new System.Drawing.Point(12, 135);
+            this.button_Exit.Location = new System.Drawing.Point(12, 220);
             this.button_Exit.Name = "button_Exit";
-            this.button_Exit.Size = new System.Drawing.Size(260, 115);
+            this.button_Exit.Size = new System.Drawing.Size(260, 30);
             this.button_Exit.TabIndex = 1;
             this.button_Exit.Text = "Exit";
             this.button_Exit.UseVisualStyleBackColor = true;
             this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label_Time
+            // 
+            this.label_Time.AutoSize = true;
+            this.label_Time.Location = new System.Drawing.Point(12, 62);
+            this.label_Time.Name = "label_Time";
+            this.label_Time.Size = new System.Drawing.Size(73, 12);
+            this.label_Time.TabIndex = 3;
+            this.label_Time.Text = "終了時間(秒)";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 142);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(152, 142);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(120, 23);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "残り時間";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0秒";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label_Time);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_Exit);
             this.Controls.Add(this.buttonHello);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +143,12 @@
         private System.Windows.Forms.Button buttonHello;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_Time;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
